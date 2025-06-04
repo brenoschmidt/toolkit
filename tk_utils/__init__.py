1d87deb6265b1f16a8e088a5bc4ecafc007dafdb
Breno Schmidt <breno.schmidt@gmail.com>
2025-06-04 20:02:18 +1000

    renamed defaults to options

--- tk_utils/__init__.py
+++ tk_utils/__init__.py
@@ -20,7 +20,7 @@ except ModuleNotFoundError as e:
             "   python tk_utils/setup.py"
             ) from e
 
-from tk_utils_core import defaults as _dflts
+from tk_utils_core import options as _dflts
 
 THIS_FILE = pathlib.Path(__file__)
 TK_UTILS_DIR = THIS_FILE.parent
@@ -28,15 +28,15 @@ PRJ_DIR = TK_UTILS_DIR.parent
 CONFIG_TOML = TK_UTILS_DIR.joinpath('config.toml')
 
 # Update and localize defaults
-tk_defaults = _dflts.load_toml_defaults(CONFIG_TOML)
-_dflts.configure(tk_defaults)
-defaults = _dflts.defaults
+tk_options = _dflts.load_toml_defaults(CONFIG_TOML)
+_dflts.configure(tk_options)
+options = _dflts.options
 
 # Path validator
 _tkpaths = tk_utils_core.pycharm.TKPaths(PRJ_DIR)
 _sysutils = tk_utils_core.pycharm.SysUtils(_tkpaths)
 
-if defaults.pycharm.validate_paths is True:
+if options.pycharm.validate_paths is True:
     _tkpaths.validate_tk_utils_init()
     _tkpaths.validate_venv()
 
